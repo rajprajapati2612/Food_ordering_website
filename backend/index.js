7526864464b8048921f815b4aba68cd056535784
rajprajapati2612 <rajprajapaticode85@gmail.com>
2026-08-21 14:37:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
  app.use(express.json());
  app.use(cookieParser());
   app.use(cors({
-   origin:'http://localhost:5173',
+   origin:'https://food-ordering-website-jyj0.onrender.com',
    credentials:true
   }))
  
