4a32d879d22a6d21b7ee03ce64099a49ea4b19fe
rajprajapati2612 <rajprajapaticode85@gmail.com>
2026-08-21 14:02:30 +0530

    Update ChangePassword.jsx

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -30,7 +30,7 @@ const ChangePassword = () => {
 
         try {
             setIsLoading(true)
-            const res = await axios.post(`http://localhost:8000/user/change-password/${email}`, {
+            const res = await axios.post(`https://food-ordering-website-backend-u9o1.onrender.com/user/change-password/${email}`, {
                 newPassword,
                 confirmPassword
             })
